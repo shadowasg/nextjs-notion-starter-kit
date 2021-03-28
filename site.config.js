4120efa8c85f4ec3f9c44f5b2feea2823922cc66
shadowasg <73029426+shadowasg@users.noreply.github.com>
2021-03-28 15:06:50 +0300

    Personal links added

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 module.exports = {
   // where it all starts -- the site's root Notion page (required)
-  rootNotionPageId: '78fc5a4b88d74b0e824e29407e9f1ec1',
+  rootNotionPageId: 'Homepage-1ddf3afe8d7b47e68bb71d2246f4f545',
 
   // if you want to restrict pages to a single notion workspace (optional)
   // (this should be a Notion ID; see the docs for how to extract this)
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Transitive Bullshit',
-  domain: 'transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Anton Spiridonov',
+  domain: 'personalpage-git-main-shadowasg.vercel.app',
+  author: 'Anton Spiridonov',
 
   // open graph metadata (optional)
-  description: 'Example site description',
-  socialImageTitle: 'Transitive Bullshit',
-  socialImageSubtitle: 'Hello World! 👋',
+  //description: 'Welcome to my homepage!',
+  //socialImageTitle: '',
+  //socialImageSubtitle: '',
 
   // social usernames (optional)
-  twitter: 'transitive_bs',
-  github: 'transitive-bullshit',
-  linkedin: 'fisch2',
+  //twitter: '',
+  //github: '',
+  //linkedin: '',
 
   // default notion icon and cover images for site-wide consistency (optional)
   // page-specific values will override these site-wide defaults
@@ -43,9 +43,10 @@ module.exports = {
   // any pages defined here will override their default URL paths
   // example:
   //
-  // pageUrlOverrides: {
-  //   '/foo': '067dd719a912471ea9a3ac10710e7fdf',
-  //   '/bar': '0be6efce9daf42688f65c76b89f8eb27'
-  // }
+  pageUrlOverrides: {
+     '/cv': 'CV-d39b4f6519fc480c98f3ea2f537e7680',
+     '/rfid': 'RFID-546ec2736baa499e9d43168c49367bcc',
+     '/conveyor': 'Low-code-05975e97efb14dfcafe013fd2037610e'
+   }
   pageUrlOverrides: null
 }
